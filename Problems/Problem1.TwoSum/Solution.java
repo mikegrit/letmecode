8a980caad879e0094fca1a3d255fede94e36847f
wumh_xmqt <mike@xmqt.com.cn>
2022-04-14 22:14:08 +0800

    + Problem 1

--- Problems/Problem1.TwoSum/Solution.java
+++ Problems/Problem1.TwoSum/Solution.java
@@ -2,21 +2,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-        public int[] twoSum(int[] nums, int target) {
+        public static int[] twoSum(int[] nums, int target) {
             Map<Integer, Integer> map = new HashMap<>();
 
             for(int i = 0; i < nums.length; i++) {
                 int n = target - nums[i];
                 //
-                if(map.containsValue(n)) {
-                    return new int[]{i, map.get(n)};
+                if(map.containsKey(n)) {
+                    System.out.println("solve! [" + i + ","+ map.get(n) + "]");
+                    return  new int[]{map.get(n), i};
                 }
-                //将数值的位置存入map
+                //将数值的位置存入map:key, value
                 map.put(nums[i], i);
             }
             System.out.println("Sorry! No solution!");
             return null;
         }
-
-
-        }
+}
